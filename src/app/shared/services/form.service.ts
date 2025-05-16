ca5ee2159f9e45adbc427afc1443a762cbee1529
U01A64C4 <g.pareja.escamilla@accenture.com>
2025-05-16 09:13:29 +0200

    Update

--- src/app/shared/services/form.service.ts
+++ src/app/shared/services/form.service.ts
@@ -3,8 +3,10 @@ import { ToastrService } from 'ngx-toastr';
 
 @Injectable({ providedIn: 'root' })
 export class FormService {
+  // Injections
   private toastr = inject(ToastrService);
 
+  // Methods
   navigateToFormError(): void {
     const invalidElement = document.querySelector('[data-invalid="true"]');
 
