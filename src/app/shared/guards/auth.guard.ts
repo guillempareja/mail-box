f59af77f997d4c65ccdabb5c0c2e46b5521bbf5c
U01A64C4 <g.pareja.escamilla@accenture.com>
2025-08-07 09:14:26 +0200

    Update

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,16 +1,18 @@
 import { inject, Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { LoginService } from '@core/services/login.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
   private router = inject(Router);
+  private loginService = inject(LoginService);
 
   canActivate(): boolean {
-    const userData = localStorage.getItem('userData');
+    const userData = this.loginService.userData();
 
-    if (userData) {
+    if (userData?.token) {
       return true;
     }
 
