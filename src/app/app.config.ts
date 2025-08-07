722e2255711ed1bef8b14291a2687c9f74fe1570
U01A64C4 <g.pareja.escamilla@accenture.com>
2025-08-07 08:59:02 +0200

    Update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptors,
+  withFetch,
+} from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideHttpCache, withHttpCacheInterceptor } from '@ngneat/cashew';
 import { customHttpInterceptor } from '@core/interceptors/http.interceptor';
@@ -22,6 +26,7 @@ export const appConfig: ApplicationConfig = {
     }),
     provideHttpClient(
       withInterceptors([withHttpCacheInterceptor(), customHttpInterceptor]),
+      withFetch(),
     ),
     provideHttpCache(),
     { provide: LOCALE_ID, useValue: 'es-ES' },
